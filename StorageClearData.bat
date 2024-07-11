32e1f681b4fce1a9a0ced0f2ed921da510bb3205
badjtsx <90721515+badjtsx@users.noreply.github.com>
2024-07-11 08:38:20 +0300

    Update StorageClearData.bat

--- StorageClearData.bat
+++ StorageClearData.bat
@@ -2,7 +2,7 @@
 cd C:\LDPlayer\LDPlayer9
 
 adb devices
-adb shell "su -c 'cd /data && dd if=/dev/zero of=fillfile bs=1048576 count=10000'"
+adb shell "su -c 'cd /data && dd if=/dev/zero of=fillfile bs=1048576 count=1024'"
 
 adb shell rm /data/fillfile
 
